0989a0106467480766d4729bab7e044ae1c4e547
Jake Cox <nicodangelo15@gmail.com>
2015-03-21 21:41:56 -0500

    Added Item and lots more houskeeping

---
+++ MyApplication/app/src/main/java/com/nicodangelo/item/Item.java
@@ -0,0 +1,84 @@
+package com.nicodangelo.item;
+
+//version 1.0
+
+public class Item {
+	String itemName;
+	int amountOfItem;
+	int warnLow;
+	boolean isSelected = false;
+	
+	public Item(String name, int amount, int low)
+	{
+		itemName = name;
+		amountOfItem = amount;
+		warnLow = low;
+	}
+   
+   public Item(String name, int amount)
+	{
+		itemName = name;
+		amountOfItem = amount;
+		warnLow = 0;
+	}
+   
+   public Item(String name)
+	{
+		itemName = name;
+		amountOfItem = 0;
+		warnLow = 0;
+	}
+	
+	public Item()
+	{
+		itemName = "";
+		amountOfItem = 0;
+		warnLow = 0;
+   }
+	
+	public String getName()
+	{
+		return itemName;
+	}
+	
+	public int getAmount()
+	{
+		return amountOfItem;
+	}
+	
+	public void setName(String name)
+	{
+		itemName = name;
+	}
+	
+	public void setAmount(int newAmount)
+	{
+		amountOfItem = newAmount;
+	}
+   
+    public int getLow()
+	{
+		return warnLow;
+	}
+	
+	public void setLow(int low)
+	{
+		warnLow = low;
+	}
+
+	public boolean getSelected()
+	{
+		return isSelected;
+	}
+
+	public void setSelected(boolean stuff)
+	{
+		isSelected = stuff;
+	}
+
+
+	
+
+	
+   
+}
--- MyApplication/app/src/main/java/com/nicodangelo/pantrie/MainPantrie.java
+++ MyApplication/app/src/main/java/com/nicodangelo/pantrie/MainPantrie.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.nicodangelo.pantrie.R;
+
 
 public class MainPantrie extends ActionBarActivity
 {
@@ -14,31 +16,5 @@ public class MainPantrie extends ActionBarActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_pantrie);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_pantrie, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
