2f74e358e41da1a2b6af4c8d9cf8e141c101642e
Jake Cox <nicodangelo15@gmail.com>
2015-03-22 01:19:07 -0500

    deleted the options bar in the listview

--- MyApplication/app/src/main/java/com/nicodangelo/list/ListMain.java
+++ MyApplication/app/src/main/java/com/nicodangelo/list/ListMain.java
@@ -22,6 +22,8 @@ import java.lang.Override;
 import java.lang.String;
 import java.util.ArrayList;
 
+import static com.nicodangelo.pantrie.R.id.editList;
+
 public class ListMain extends ListActivity
 {
 
@@ -128,29 +130,4 @@ public class ListMain extends ListActivity
         super.onListItemClick(l, v, position, id);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.id., menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
